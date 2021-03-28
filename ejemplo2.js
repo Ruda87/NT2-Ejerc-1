6c9428b668c48062140fcd02dc4f8524d61e6303
Ruda87 <d.a.ruiz87@hotmail.com>
2021-03-28 19:20:14 -0300

    Daniel Ruiz

---
+++ ejemplo2.js
@@ -0,0 +1,12 @@
+
+var obj = [{ Nombre: 'Adrian', Apellido: 'Caceres' },
+        { Nombre: 'AlumnoNombre', Apellido: 'ApellidoAlumno' },
+        { Nombre: 'Christian', Apellido: 'Dellarosa' },
+        { Nombre: 'Facundo', Apellido: 'Rossaroli' },
+        { Nombre: 'Ivan', Apellido: 'Maldonado' },
+        { Nombre: 'Lourdes Paloma', Apellido: 'Herrera'},
+	    { Nombre: 'Juan Manuel', Apellido: 'Castillo'},
+        { Nombre: 'Daniel Alberto', Apellido: 'Ruiz'}
+]
+
+console.log(obj);
